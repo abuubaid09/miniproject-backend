5a73c8e6440ca3b80a43fc6c07bd8b77dee202ab
abuubaid09 <rickyadrian65@gmail.com>
2022-01-24 23:41:41 +0700

    update server.js untuk import productController dan productModel

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const express = require('express');
 require('dotenv').config();
 const sequelize = require('./library/database.js');
-// const connection = require('./library/database.js');
+
 require('./models/merchantModel');
+require('./models/productModel');
+
 const merchantRoutes = require('./routes/merchantRoutes');
+const productRoutes = require('./routes/productRoutes');
+
 const app = express();
 
 app.use(express.json());
 
 app.use('/merchant',merchantRoutes);
+app.use('/product',productRoutes);
 
 const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, console.log(`Server running on port ${PORT}`));
-
-// connection.connect(function(error){
-//     if(!!error){
-//       console.log("Koneksi Database gagal");
-//     }else{
-//       console.log('Koneksi Database Berhasil!');
-//     }
-//   })
 
 sequelize.authenticate().then(()=>{
     sequelize.sync().then(()=>{
